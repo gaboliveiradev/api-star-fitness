007df328d6e7a1b6f574c99e59182d2e315c6f6d
gaboliveiradev <g38789138@gmail.com>
2023-09-08 09:10:50 -0300

    feat: route delete

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -8,24 +8,35 @@ use Illuminate\Support\Facades\Auth;
 
 class TypeController extends Controller
 {
-    private TypeModel $typeModel;
-
-    public function __construct()
-    {
-        $this->typeModel = new TypeModel();
-    }
-
     public function getAll()
     {
-        $type = TypeModel::all(['*']);
+        $type = TypeModel::all();
 
         return $this->success('Types', $type, 200);
     }
 
     public function create(CreateTypeRequest $request)
     {
-        $type = $this->typeModel::create($request->all());
+        $type = TypeModel::create($request->all());
 
         return $this->success('Type Created', $type, 201);
+    }
+
+    public function delete($id) 
+    {
+        $type = TypeModel::where('id', $id)->first();
+
+        if (!$type) {
+            return $this->error('Plan Not Found', 404);
+        }
+
+        if (!$type->active) {
+            return $this->error('This plan is already deactivated.', 422);
+        }
+
+        $type->active = false;
+        $type->save();
+
+        return $this->success('Plan Deactivated Successfully', $type, 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,8 +30,9 @@ use App\Http\Controllers\AddressController;
 
     // Type
 
-    Route::post('/type', [TypeController::class, 'create']);
     Route::get('/type', [TypeController::class, 'getAll']);
+    Route::post('/type', [TypeController::class, 'create']);
+    Route::delete('/type/{id}', [TypeController::class, 'delete']);
 //});
 
 // Login
