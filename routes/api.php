58ca147e65ad42a15232b23e0fac4e446a09719f
Gabriel Oliveira <55067364+gaboliveiradev@users.noreply.github.com>
2023-10-21 09:06:41 -0300

    Atualizar o api.php
    Route billing

--- routes/api.php
+++ routes/api.php
@@ -33,6 +33,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // Billing
 
     Route::post('/billing', [BillingController::class, 'create']);
+    Route::get('/billing', [BillingController::class, 'getAllByIdUser']);
 
     // Type
 
