3864851d54a202861e8a82bbd47418f49d9d9dc4
gaboliveiradev <g38789138@gmail.com>
2023-11-01 10:00:12 -0300

    fix: charge double type in request measurement

--- app/Http/Requests/CreateMeasurementRequest.php
+++ app/Http/Requests/CreateMeasurementRequest.php
@@ -9,16 +9,16 @@ class CreateMeasurementRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "chest" => ['double'], 
-            "glute" => ['double'], 
-            "left_arm" => ['double'], 
-            "right_arm" => ['double'], 
-            "left_calf" => ['double'], 
-            "right_calf" => ['double'], 
-            "left_forearm" => ['double'], 
-            "right_forearm" => ['double'], 
-            "left_quadriceps" => ['double'], 
-            "right_quadriceps" => ['double'], 
+            "chest" => [''], 
+            "glute" => [''], 
+            "left_arm" => [''], 
+            "right_arm" => [''], 
+            "left_calf" => [''], 
+            "right_calf" => [''], 
+            "left_forearm" => [''], 
+            "right_forearm" => [''], 
+            "left_quadriceps" => [''], 
+            "right_quadriceps" => [''], 
             "id_evolution" => ['required', 'string'], 
         ];
     }
